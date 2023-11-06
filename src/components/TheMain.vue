9a03a5245c87a04b829f83fbc310c238d2307fa3
developer2239 <42111134+developer2239@users.noreply.github.com>
2023-11-06 18:54:34 +0900

    描画側大枠作成

---
+++ src/components/TheMain.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+<template>
+  <div class="wrapper">
+    <h2>TheMainContent</h2>
+  </div>
+</template>
+<style scoped>
+.wrapper {
+  flex-grow: 1;
+  border-left: 1px solid gray;
+  border-right: 1px solid gray;
+}
+</style>
